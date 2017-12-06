c3006314c145e70d43e720dd8f5d39e7e701d972
Kijko <kijkongs@gmail.com>
2017-12-06 20:09:01 +0100

    Tak

--- src/kni/webshop/SimpleController.java
+++ src/kni/webshop/SimpleController.java
@@ -1,12 +1,21 @@
 package kni.webshop;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SimpleController {
 
+	 @RequestMapping("/")   
+	 public String welcome(Model model) { 
+		 model.addAttribute("greeting", "SIEMA!");     
+		 model.addAttribute("tagline", "Policja wrog");
+     return "siema";   
+     }
+	
+	
 	@RequestMapping("/welcome")
 	public ModelAndView simpleMethod() {
 		String message = "<br><div style='text-align:center;'>\"\r\n" 
--- target/kni-web-shop-0.0.1-SNAPSHOT/resources/javascript.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($) {
- 
-	$('#jsmessage').html("<h4>Wiadomosc z pliku javascript.js</h4>")
- 
-});
